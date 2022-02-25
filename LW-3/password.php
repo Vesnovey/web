6bd3db731224b38650a8cd5420b220ac2712074e
Nikita <ilinna2005@gmail.com>
2022-02-25 10:56:13 +0300

    final version

--- LW-3/password.php
+++ LW-3/password.php
@@ -1,20 +1,38 @@
 <?php
+    header("Content-Type: text/plain");
+
     $string = $_GET['password'];
     if (empty($string)) {
         echo("ОшИБКАААА! Нет пароля");
     }
     else {
         $strength = 0;
-        $strength += 4 * length($string);
-        $count_digit  = strlen(preg_replace('/[^\d]/','',$string));
+        $strength += 4 * strlen($string);
+        $count_digit = strlen(preg_replace('/[^\d]/', '', $string));
         $strength += $count_digit * 4;
-        if (ctype_upper($string)){
-            $upper = 0;
-            $lower = 0;
-            foreach(preg_split('~~u', $string, -1, PREG_SPLIT_NO_EMPTY) as $item) {
-                if (preg_match('~[A-ZА-ЯЁ]~u', $item)) ++$upper;
-                else if (preg_match('~[a-zа-яё]~u', $item)) ++$lower;
-                echo $upper;
+        if (ctype_upper($string)) {
+            $high = 0;
+            for ($i = 0; $i < strlen($string); $i++) {
+                if (ctype_upper(substr($string, $i, 1))) {
+                    $high++;
+                }
+            }
+            $strength += (strlen($string) - $high) * 2;
+        }
+
+        if (ctype_alpha($string) or ctype_digit($string)){
+            $strength -= strlen($string);
+            }
+        $c = 0;
+        for ($i = 0; $i < strlen($string); $i++) {
+            for ($m = 0; $m < strlen($string); $m++){
+                if ((substr($string, $i, 1)) == (substr($string, $m, 1))){
+                    $c++;
+                }
             }
         }
+        $strength -= $c;
+        echo "Cила пароля: ", $strength;
+
+
     }
